4c2cd7150d86fae04aadddc1d388984eb1588adb
Raj Kujur <rajkujur@gmail.com>
2017-11-19 14:04:46 +1100

    E2E Done

---
+++ endtoendautomation/InputData/ProductList.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ProductInfo: {
+        'Bose QC35 ii': {productName: 'Bose QC35 ii', itemNumber: '1'},
+        'Xbox One X 1TB Console': {productName: 'Xbox One X 1TB Console', itemNumber: '1'},
+        'MacBook Pro 13.3-inch 2.3GHz 128GB Space Grey': {productName: 'MacBook Pro 13.3-inch 2.3GHz 128GB Space Grey', itemNumber: '1'}
+    }
+}
--- endtoendautomation/specs/ebay.search.add.validate.cart.spec.js
+++ endtoendautomation/specs/ebay.search.add.validate.cart.spec.js
@@ -1,55 +1,58 @@
+var productList = require('../InputData/ProductList.js');
+var using = require('/Users/kujurra_local/node_modules/jasmine-data-provider');
 var ebayHomePage = require('../pages/ebay.homepage.js'),
     searchResultPage = require('../pages/ebay.searchResult.Page.js');
     productPage = require('../pages/ebay.product.page.js');
     cartPage = require('../pages/ebay.cart.page.js');
     EC = protractor.ExpectedConditions;
-describe('Search 2 products then add them and validate cart',function(){
-  var EbayHomePage = new ebayHomePage(),
-      SearchResultPage = new searchResultPage();
-      ProductPage = new productPage();
-      CartPage = new cartPage();
-    beforeAll(function(){
-      browser.get('https://www.ebay.com.au/');
-      expect(browser.getCurrentUrl()).toMatch('ebay.com.au');
+using(productList.ProductInfo, function (data, description) {
+  describe('Search '+ description + ' product then add them and validate cart',function(){
+    var EbayHomePage = new ebayHomePage(),
+        SearchResultPage = new searchResultPage();
+        ProductPage = new productPage();
+        CartPage = new cartPage();
+      beforeAll(function(){
+        browser.get('https://www.ebay.com.au/');
+        expect(browser.getCurrentUrl()).toMatch('ebay.com.au');
+      });
+
+    describe('When the ebay site is loaded', function(){
+      it('Should search product in ebay site', function(){
+        EbayHomePage.setSearchInputBox(data.productName);
+        expect(EbayHomePage.searchInputBox.getAttribute('value')).toMatch(data.productName);
+      });
     });
 
-  describe('When the ebay site is loaded', function(){
-    it('Should search product in ebay site', function(){
-      EbayHomePage.setSearchInputBox('Bose QC35 ii');
-      expect(EbayHomePage.searchInputBox.getAttribute('value')).toMatch('Bose QC35 ii');
+    describe('When the product to be searched is set', function(){
+      it('Should click on search button', function(){
+        EbayHomePage.clickSearchButton();
+        browser.wait(EC.presenceOf(SearchResultPage.searchResult), 5000);
+        expect(SearchResultPage.searchResult.isPresent()).toBeTruthy;
+      });
+    });
+
+    describe('When the product is searched', function(){
+      it('Should select the first product', function(){
+        SearchResultPage.selectItemNumber(data.itemNumber);
+        browser.wait(EC.presenceOf(ProductPage.productName), 5000);
+        expect(ProductPage.productName.getText()).toMatch(data.productName);
+      });
+    });
+
+    describe('When the product is selected', function(){
+      it('Should add to cart', function(){
+        ProductPage.clickAddtoCartButton();
+        browser.wait(EC.presenceOf(ProductPage.noThanksButton), 5000);
+        ProductPage.clickNoThanksButton();
+        browser.wait(EC.presenceOf(CartPage.cartPageHeader), 5000);
+        expect(CartPage.cartPageHeader.isPresent()).toBeTruthy;
+      });
+    });
+
+    describe('When the product is added to cart', function(){
+      it('Validate if item is present', function(){
+        expect(CartPage.cartItemValidation(data.productName)).toBeTruthy;
+      });
     });
   });
-
-  describe('When the product to be searched is set', function(){
-    it('Should click on search button', function(){
-      EbayHomePage.clickSearchButton();
-      browser.wait(EC.presenceOf(SearchResultPage.searchResult), 5000);
-      expect(SearchResultPage.searchResult.isPresent()).toBeTruthy;
-    });
-  });
-
-  describe('When the product is searched', function(){
-    it('Should select the first product', function(){
-      SearchResultPage.selectItemNumber('1');
-      browser.wait(EC.presenceOf(ProductPage.productName), 5000);
-      expect(ProductPage.productName.getText()).toMatch('Bose QC35 ii');
-    });
-  });
-
-  describe('When the product is selected', function(){
-    it('Should add to cart', function(){
-      ProductPage.clickAddtoCartButton();
-      browser.wait(EC.presenceOf(ProductPage.noThanksButton), 5000);
-      ProductPage.clickNoThanksButton();
-      browser.wait(EC.presenceOf(CartPage.cartPageHeader), 5000);
-      expect(CartPage.cartPageHeader.isPresent()).toBeTruthy;
-    });
-  });
-
-  describe('When the product is added to cart', function(){
-    it('Validate if item is present', function(){
-      expect(CartPage.cartItemValidation('Bose QC35 ii')).toBeTruthy;
-    });
-  });
-
 });
