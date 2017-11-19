9ec94b7ca54772142c905ad247b4445bde4bf6d6
Raj Kujur <rajkujur@gmail.com>
2017-11-19 12:50:10 +1100

    E2E done

--- conf.js
+++ conf.js
@@ -3,6 +3,9 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   multiCapabilities: [{
           'browserName': 'firefox'
+        },
+        {
+          'browserName': 'chrome'
         }],
   specs: ['./**/*.spec.js'],
   framework: 'jasmine2',
